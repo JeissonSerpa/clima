5be9301f160abade94a9a331b4c2e85df036a929
Jeisson Serpa <jeisson-serpa@outlook.com>
2020-08-10 20:03:43 -0500

    Updates

---
+++ precache-manifest.e4d6ad3f86d5b02c006bb2c003993c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101b19099c4abf9d53eb972ad86fff3c",
+    "url": "/clima/index.html"
+  },
+  {
+    "revision": "2e1e14470583e4ce321d",
+    "url": "/clima/static/css/main.99037991.chunk.css"
+  },
+  {
+    "revision": "af68415d5811f16d4f9d",
+    "url": "/clima/static/js/2.8cd2a3e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clima/static/js/2.8cd2a3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1e14470583e4ce321d",
+    "url": "/clima/static/js/main.a99a3fd8.chunk.js"
+  },
+  {
+    "revision": "1b9741051ce960f4a1d1",
+    "url": "/clima/static/js/runtime-main.12c63bf1.js"
+  }
+]);
